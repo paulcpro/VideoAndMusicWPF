8d1f12c67167093c0fa70c6613eb1eadf4305322
paulcpro <44520400+paulcpro@users.noreply.github.com>
2021-01-18 10:17:02 +0100

    Base of the structure

---
+++ Custom Controls/SearchBar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoAndMusicWPF.Custom_Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour SearchBar.xaml
+    /// </summary>
+    public partial class SearchBar : UserControl
+    {
+
+        #region Constructors
+
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+
+        #endregion //Constructors
+
+        #region DependencyProperties
+
+        public string SearchText
+        {
+            get { return (string)GetValue(SearchTextProperty); }
+            set { SetValue(SearchTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchTextProperty =
+            DependencyProperty.Register("SearchText", typeof(string), typeof(SearchBar));
+
+        #endregion //DependencyProperties
+
+    }
+}
---
+++ Custom Controls/UserProfileButton.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoAndMusicWPF.Custom_Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour UserProfileButton.xaml
+    /// </summary>
+    public partial class UserProfileButton : UserControl
+    {
+
+        #region Constructors
+
+        public UserProfileButton()
+        {
+            InitializeComponent();
+        }
+
+        #endregion //Constructors
+
+        #region DependencyProperties
+
+        public Uri ImageSource
+        {
+            get { return (Uri)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(Uri), typeof(UserProfileButton));
+
+        public string UserText
+        {
+            get { return (string)GetValue(UserTextProperty); }
+            set { SetValue(UserTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UserText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UserTextProperty =
+            DependencyProperty.Register("UserText", typeof(string), typeof(UserProfileButton));
+
+        #endregion //DependencyProperties
+
+    }
+}
